8274de20f2d2fcf23710c71b4c3af6534b030e7a
GenelRoyal <177992123+GenelRoyal@users.noreply.github.com>
2024-09-18 14:33:57 +0000

    [Finish] 課題作成Day3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,11 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
+  before_action :is_matching_lodin_user, only: [:edit, :update]
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path(@book)
   end
 
   def index
@@ -19,6 +17,17 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @user = @book.user
+    @book_new = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
   end
 
   def destroy
@@ -31,5 +40,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    book = book.find(params[:id])
+    user = User.find(book.user_id)
+    unless user.id == current_user_id
+      redirect_to books_path
+    end
   end
 end
